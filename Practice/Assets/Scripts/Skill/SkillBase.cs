4da23a81371837683b413b6cfd957c75cd1c53fe
rkrk0285 <82379491+rkrk0285@users.noreply.github.com>
2024-10-08 16:29:08 +0900

    [Feat] RPG 요소 강화
    -

--- Practice/Assets/Scripts/Skill/SkillBase.cs
+++ Practice/Assets/Scripts/Skill/SkillBase.cs
@@ -4,5 +4,35 @@ using UnityEngine;
 
 public class SkillBase : MonoBehaviour
 {
-    public virtual void Activate() { }    
+    private float ballRadius;
+    protected virtual void Start()
+    {
+        ballRadius = transform.localScale.x / 2;
+    }    
+    protected void DrawLine(Vector2 dir)
+    {
+        LineRenderer lr = GetComponent<LineRenderer>();
+        RaycastHit2D hit = GetCircleCastHit(transform.position, dir, gameObject);
+
+        lr.positionCount = 2;
+        lr.SetPosition(0, transform.position);
+        lr.SetPosition(1, hit.point + hit.normal * ballRadius);
+
+        lr.startColor = Color.white;
+        lr.endColor = Color.white;
+        lr.enabled = true;
+    }
+
+    protected RaycastHit2D GetCircleCastHit(Vector2 pos, Vector2 dir, GameObject obj)
+    {
+        RaycastHit2D[] hit = Physics2D.CircleCastAll(pos, ballRadius, dir, 100f);
+        for (int i = 0; i < hit.Length; i++)
+        {
+            if (hit[i].collider.gameObject != obj)
+                return hit[i];
+        }
+        return new RaycastHit2D();
+    }
+
+    public virtual void Activate() { }
 }
