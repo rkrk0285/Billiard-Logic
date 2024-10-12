2c89fd26d7874d18457f2eb7affb494fcc8a0b9f
rkrk0285 <82379491+rkrk0285@users.noreply.github.com>
2024-10-12 12:49:42 +0900

    [Feat] Move Test
    - 이동, 파워 랜덤 기능 구현.

--- Practice/Assets/Scripts/MonsterController.cs
+++ Practice/Assets/Scripts/MonsterController.cs
@@ -22,20 +22,32 @@ public class MonsterController : MonoBehaviour
     [SerializeField] private float StopMagnitude;
     [SerializeField] private float BreakDrag;
 
+    [Space]
+    [Range(0,1)][SerializeField] private float ChangeDirectionSpeed;
+    [Tooltip("This parameter is added to the Power Offset Parameter once per update. The Power Offset ranges from 0 to 1.")]
+    [Range(0, 0.1f)][SerializeField] private float ChangeDirectionPower;
+
+    [SerializeField] private float MaxPower;
+    [SerializeField] private float MinPower;
+
     [Header("Current Parameters")]
     private float ballRadius;
-    private float currentPower;
+    private float currPower;
     private float epsilon;
     private int skipTurn;
     private Vector2 currVelocity;
     private E_MonsterState monsterState;
+    
+    private float angleOffset;
+    private float powerOffset;
+    private bool angleIncrease;     
 
     [Header("Components")]
     [SerializeField] private MonsterStat monsterStat;
 
     private Rigidbody2D rb;
     private BallStat ballStat;
-    private LineRenderer lr;
+    private LineRenderer lr;   
     
     private void Start()
     {
@@ -49,7 +61,13 @@ public class MonsterController : MonoBehaviour
 
         ballRadius = transform.localScale.x / 2;
         currVelocity = rb.velocity;
-        currentPower = Power + PlayerPrefs.GetFloat("Power", 0);
+        currPower = 0f;
+        //currPower = Power + PlayerPrefs.GetFloat("Power", 0);        
+
+        angleOffset = 0f;
+        powerOffset = 0f;
+        angleIncrease = true;        
+
         skipTurn = 0;
         monsterState = E_MonsterState.Default;
     }
@@ -85,16 +103,17 @@ public class MonsterController : MonoBehaviour
         {
             case E_MonsterState.Ready:
                 Vector2 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                Vector2 dir = worldPos - new Vector2(transform.position.x, transform.position.y);
-
+                Vector2 dir = worldPos - new Vector2(transform.position.x, transform.position.y);                                
                 if (CheckAnyTurnToSkip())
-                    break;                
+                    break;
+
+                dir = ChangeAngleByDirection(dir);
+                currPower = ChangePower();
 
                 if (Input.GetMouseButtonDown(0))
                     MoveMonster(dir);                    
                 else
-                    DrawLine(dir);
-
+                    DrawLine(dir);               
                 break;
             case E_MonsterState.Moving:
                 if (rb.velocity.magnitude == 0)
@@ -107,27 +126,67 @@ public class MonsterController : MonoBehaviour
     }
     protected void MoveMonster(Vector2 dir)
     {
-        rb.velocity = dir.normalized * currentPower;
+        rb.velocity = dir.normalized * currPower;
         rb.drag = DefaultDrag;
-
+        Debug.Log(currPower);
         monsterState = E_MonsterState.Moving;
         lr.enabled = false;
-    }    
+    }
+
+    private Vector2 ChangeAngleByDirection(Vector2 dir)
+    {                    
+        if (angleIncrease)
+        {
+            angleOffset += ChangeDirectionSpeed;
+            if (angleOffset > 45f)
+            {
+                angleOffset = 45f;
+                angleIncrease = false;
+            }
+        }
+        else
+        {
+            angleOffset -= ChangeDirectionSpeed;
+            if (angleOffset < -45f)
+            {
+                angleOffset = -45f;
+                angleIncrease = true;
+            }
+        }
+
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg + angleOffset;
+        float radian = angle * Mathf.Deg2Rad;
+        return new Vector2 (Mathf.Cos(radian), Mathf.Sin(radian));       
+    }
+    private float ChangePower()
+    {
+        powerOffset += ChangeDirectionPower;
+        if (powerOffset > 1f)
+            powerOffset = 0;
+        
+        return MinPower + (MaxPower - MinPower) * powerOffset;
+    }
+    private static float NormalizeAngle(float angle)
+    {
+        angle %= 360f;
+        if (angle < 0)
+            angle += 360f;
+        return angle;
+    }
     private void DrawLine(Vector2 dir)
     {        
-        RaycastHit2D hit = GetCircleCastHit(transform.position, dir, gameObject);        
-        
+        RaycastHit2D hit = GetCircleCastHit(transform.position, dir, gameObject);
+
         lr.positionCount = 2;
         lr.SetPosition(0, transform.position);
-        lr.SetPosition(1, hit.point + hit.normal * ballRadius);
-
+        lr.SetPosition(1, transform.position + (Vector3)dir.normalized * powerOffset * 10);        
         lr.startColor = Color.white;
         lr.endColor = Color.white;
-        lr.enabled = true;        
+        lr.enabled = true;
     }
     protected RaycastHit2D GetCircleCastHit(Vector2 pos, Vector2 dir, GameObject obj)
     {
-        RaycastHit2D[] hit = Physics2D.CircleCastAll(pos, ballRadius, dir, 100f);
+        RaycastHit2D[] hit = Physics2D.CircleCastAll(pos, ballRadius, dir, 5f);
         for (int i = 0; i < hit.Length; i++)
         {
             if (hit[i].collider.gameObject != obj)
@@ -153,7 +212,7 @@ public class MonsterController : MonoBehaviour
     }
     public void ResetEndPhysicsParameter()
     {
-        currentPower = Power;
+        currPower = Power;
         rb.mass = Mass;
         rb.drag = Mathf.Max(DefaultDrag + PlayerPrefs.GetFloat("Drag", 0), 0);
     }
