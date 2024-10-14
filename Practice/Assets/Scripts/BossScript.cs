1cacb8e2d7169a45cf147e318722a4cffd55aca5
dkssu46 <80806798+dkssu46@users.noreply.github.com>
2024-10-14 12:26:20 +0900

    Boss Temp

---
+++ Practice/Assets/Scripts/BossScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossScript : MonoBehaviour
+{
+    [SerializeField] GameObject[] skills;
+    [SerializeField] GameObject player;
+    [SerializeField] float skillDelay = 2;
+    [SerializeField] float skilltime = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(UseSkill());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookAtPlayer();
+    }
+
+    // 플레이어를 바라보는 함수
+    void LookAtPlayer()
+    {
+        // 플레이어와 보스의 위치 차이를 계산
+        Vector3 direction = transform.position - player.transform.position;
+        // Z축 회전각을 계산 (2D 환경)
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        // 회전 각도를 설정, 기본적으로 아래(-90도)를 보고 있으므로 추가로 -90도 회전
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
+    }
+
+    IEnumerator UseSkill()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(skillDelay);
+            int index = Random.Range(0, skills.Length);
+            skills[index].SetActive(true);
+            yield return new WaitForSeconds(skilltime);
+            skills[index].SetActive(false);
+        }
+    }
+}
